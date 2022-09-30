ce1fae718ca05cdb8b99a1120602cb00a8f41aee
AinaRapawi <19b2130@ubd.edu.bn>
2022-09-30 18:30:18 +0800

    Upload solution.m file
    Code generates random sparse matrix with a zeros border and a matrix that replaces each element with the sum of its neighbours

---
+++ solution.m
@@ -0,0 +1,23 @@
+%% Conway's Game of Life
+
+% Generating random 1st Generation
+g = 10; % grid size
+d = 0.3; % density of nonzeros
+
+x = sprand(g-2,g-2,d);
+gen1 = spones(x); % converting nonzeros to ones
+
+% Creating a zeros border
+gen = zeros(g,g);
+gen(2:g-1,2:g-1) = gen1;
+
+% Displaying initial generation
+full(gen)
+
+% Replacing each element with the sum of its neighbours
+i = 2:g-1;
+j = 2:g-1;
+gen(i,j) = gen(i-1,j+1) + gen(i-1,j) + gen(i-1,j-1) + gen(i,j+1) + gen(i,j-1) + gen(i+1,j+1) + gen(i+1,j) + gen(i+1,j-1);
+full(gen)
+
+%% Implement game rules
