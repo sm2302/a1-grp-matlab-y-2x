c3ec90eeac85cb8d04a96a4d17b7a9d98fa9b966
AiniAbd <111748570+AiniAbd@users.noreply.github.com>
2022-09-30 19:26:37 +0800

    Update solution.m
    updated Conway's Game of Life

--- solution.m
+++ solution.m
@@ -1,8 +1,9 @@
 %% Conway's Game of Life
 
 % Generating random 1st Generation
-g = 10; % grid size
-d = 0.3; % density of nonzeros
+g = 50; % grid size
+d = 0.1; % density of nonzeros
+n = 50
 
 x = sprand(g-2,g-2,d);
 gen1 = spones(x); % converting nonzeros to ones
@@ -11,13 +12,21 @@ gen1 = spones(x); % converting nonzeros to ones
 gen = zeros(g,g);
 gen(2:g-1,2:g-1) = gen1;
 
-% Displaying initial generation
-full(gen)
+for t = 1:n
+  
+spy(gen,'.m',12) % plotting sparsity pattern of matrix with colour red, size 3
+title(num2str(t)) % converts a numeric array into a character array that represents the numbers
+drawnow
+pause(0.2)
 
 % Replacing each element with the sum of its neighbours
 i = 2:g-1;
 j = 2:g-1;
 gen(i,j) = gen(i-1,j+1) + gen(i-1,j) + gen(i-1,j-1) + gen(i,j+1) + gen(i,j-1) + gen(i+1,j+1) + gen(i+1,j) + gen(i+1,j-1);
-full(gen)
 
-%% Implement game rules
+% Implement game rules
+gen(gen < 2) = 0;
+gen(gen > 3) = 0;
+gen(gen == 2 | gen == 3) = 1;
+
+end
